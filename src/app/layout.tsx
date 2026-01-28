31fa74232103f0cf3684ad7b3c1886c2e48c3094
feriumaha <andikaferi10@gmail.com>
2026-01-28 11:05:58 +0700

    penambahan speedinsight next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${jetbrains.className} bg-[#0B0F19] text-white`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
